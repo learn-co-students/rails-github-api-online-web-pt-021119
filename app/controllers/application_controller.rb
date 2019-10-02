7e9c5f29ed64e7316ef19b24528d0446e31fb81d
github username <github email address>
2019-10-01 21:12:06 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,16 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = ENV['CLIENT_ID']
+    state = ENV['CLIENT_SECRET']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    github_url = "https://github.com/login/oauth/authorize?client_id#{client_id}&redirect_uri=#{redirect_uri}&state=#{state}"
+    # binding.pry
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
+    # binding.pry
+    !!session[:token]
   end
 end
